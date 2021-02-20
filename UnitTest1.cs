6e6327b5fa58c508c64efddef8766a446feb82d6
Yuchia.Wei <h42279@gmail.com>
2021-02-20 15:36:51 +0800

    Add First Case

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,13 +1,53 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
 
 namespace TddWork
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IBudgetRepo _budgetRepo;
+        private BudgetCalculator _budget;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            this._budgetRepo = Substitute.For<IBudgetRepo>();
+            this._budget = new BudgetCalculator();
         }
+
+        [TestMethod]
+        public void QueryDaily()
+        {
+            this._budgetRepo.GetAll()
+                .Returns(new List<Budget>() { new Budget() { YearMonth = "202101", Amount = 31 } });
+
+            var startDateTime = new DateTime(2021, 01, 01);
+            var endDateTime = new DateTime(2021, 01, 01);
+            var result = this._budget.Query(startDateTime, endDateTime);
+
+            Assert.AreEqual(31, result);
+        }
+    }
+
+    public class Budget
+    {
+        public int Amount { get; set; }
+        public string YearMonth { get; set; }
+    }
+
+    public class BudgetCalculator
+    {
+        public decimal Query(DateTime startDateTime, DateTime endDateTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IBudgetRepo
+    {
+        List<Budget> GetAll();
     }
 }
