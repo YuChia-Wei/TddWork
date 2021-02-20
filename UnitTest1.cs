5c94596169a8c42bfed82dbda5179f76f1913482
Yuchia.Wei <h42279@gmail.com>
2021-02-20 15:23:14 +0800

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddWork
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
